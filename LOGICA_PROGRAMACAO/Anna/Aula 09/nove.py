f5dea62f0c85ab83d347ea656e4d6b442bfddc59
Anna Concolato <concolatoanna@gmail.com>
2026-05-13 11:29:14 -0300

    Lista de exercicios corrindo erros não finalizado

--- LOGICA_PROGRAMACAO/Anna/Aula 09/nove.py
+++ LOGICA_PROGRAMACAO/Anna/Aula 09/nove.py
@@ -1,1 +1,62 @@
-print("Bem-vindo ao GitHub")
+# print("Bem-vindo ao GitHub")
+
+
+# 1. O Problema da Idade
+# idade = input("Digite sua idade: ")
+# if idade >= 18:
+# print("Você é maior de idade.")
+# corrigido
+# idade = int(input("Digite sua idade"))
+# if idade >= 18:
+#     print("Você é maior de idade")
+# melhorado
+# idade = int(input("Digite sua idade"))
+# if idade >= 18:
+#     print("Você é maior de idade.")
+# elif idade <= 17:
+#     print("Você é menor de idade.")
+
+# 2. A Escrita Fiel
+# nome = "Mariana"
+# print("Seja bem-vinda, nome!")
+# corrigido
+# nome = "Mariana"
+# print(f"Seja Bem-Vinda, {nome}")
+# melhorado
+# nome = input("Qual seu nome?")
+# print(f"Seja Bem-Vindo {nome}")
+
+# 3. Falta de Espaço
+# numero = 10
+# if numero > 5:
+# print("O número é maior que cinco.")
+# else:
+# print("O número é menor ou igual a cinco.")
+# corrigido
+# numero = 10 
+# if numero > 5:
+#     print("O número é maior que cinco")
+# else:
+#     print("O número é menor ou igual a cinco")
+# melhorado
+# numero = input("Digite um número")
+# if numero > "5":
+#     print("O número é maior que cinco")
+# else:
+#     print("O número é menor que cinco")
+
+
+# 4. Esquecimento Fatal
+# usuario = "aluno123"
+# if usuario == "aluno123"
+# print("Login realizado com sucesso.")
+# corrigido
+# usuario = "aluno123"
+# if usuario == "aluno123":
+#    print("Login realizado com sucesso.")
+# melhorado
+# usuario = input("Digite seu nome de usúario")
+# if usuario == "aluno123":
+#   print("Login realizado com sucesso.")
+# else:
+#   print("Login incoreto.")
