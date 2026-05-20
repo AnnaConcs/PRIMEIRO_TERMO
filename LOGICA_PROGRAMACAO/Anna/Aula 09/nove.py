ca20c0fb6928ea93b305872477f161d4074bd5ec
Anna Concolato <concolatoanna@gmail.com>
2026-05-20 08:55:03 -0300

    finalizando a lista

--- LOGICA_PROGRAMACAO/Anna/Aula 09/nove.py
+++ LOGICA_PROGRAMACAO/Anna/Aula 09/nove.py
@@ -60,3 +60,89 @@
 #   print("Login realizado com sucesso.")
 # else:
 #   print("Login incoreto.")
+
+
+# 5. Atribuição vs. Comparação
+# clima = "ensolarado"
+# if clima = "chuvoso":
+# print("Leve um guarda-chuva!")
+# corrigido
+# clima = "chuvoso"
+# if clima == "chuvoso":
+#    print("Leve um guarda chuva!")
+# melhorado
+# clima = input("Como está o clima hoje?:")
+# clima = "chuvoso"
+# if clima == "chuvoso":
+#    print("Leve um guarda-chuva!")
+
+
+# 6. Misturando Alhos com Bugalhos
+# pontos = 50
+# print("Parabéns! Você fez " + pontos + " pontos.")
+# corrigido
+# pontos = 50
+# print(f"Parabéns! Você fez {pontos} pontos")
+# melhorado
+# pontos = int(input("Quantos pontos você fez?:"))
+# print(f"Parabéns! Você fez mais {pontos} pontos. :)")
+
+
+# 7. A Ordem dos Fatores
+# O sistema deve dar "Excelente" para notas 9 ou 10.
+# nota = 9.5
+# if nota >= 7:
+# print("Aprovado")
+# elif nota >= 9:
+# print("Excelente!")
+# corrigido
+# nota = 8
+# if nota >= 9:
+#     print("Excelente!")
+# elif nota >= 7:
+#     print("Aprovado")
+# melhorado
+# qual_sua_nota = float(input("Qual seria sua nota?"))
+# if qual_sua_nota >= 7:
+#     print("Você foi aprovado!")
+# elif qual_sua_nota >= 9:
+#     print("Você foi Excelente!")
+# else:
+#     print("Recuperação! Melhore sua nota.")
+
+# 8. O Contador de 1 a 5
+# Objetivo: Mostrar na tela os números 1, 2, 3, 4 e 5.
+# for i in range(5):
+# print(i)
+# corrigido
+# for i in range (6):
+#     print (i)
+# melhorado
+# i = int(input("Qual número você quer contar?:"))
+# for i in range (i):
+#     print (i)
+
+# 9. O Loop Eterno
+# tentativas = 1
+# while tentativas <= 3:
+# print("Tentando conectar...")
+# O código deveria parar após 3 tentativas
+# corrigido e melhorado
+# tentativas = 1
+# while tentativas <= 3:
+#     print("Tentando conectar...")
+#     tentativas += 1
+# print("Tentativas esgotadas. A conexão falhou!")
+
+
+# 10. A Senha Teimosa
+# O programa deve pedir a senha até que o usuário digite "python123"
+# senha = ""
+# while senha == "python123":
+# senha = input("Digite a senha secreta: ")
+# print("Acesso concedido!")
+# corrigido e melhorado
+# senha = input(f"Digite a senha secreta:")
+# while senha == "python123":
+#  senha = input("Digite a senha secreta: ")
+#  print("Acesso concecido!")
